8ba5c2461c7414172ca0ade2c72c2031f7eb846e
nadiwe <nadia.west@hispeed.ch>
2022-02-16 12:21:34 +0100

    porfolio1366

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+var element = document.getElementById('portfolio');
+
+lottie.loadAnimation({
+    container: element, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: '/dataportfolio.json' // the path to the animation json
+});
+
+
+
+
+
 
 var element = document.getElementById('lottie');
 
