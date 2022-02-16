ebe6949f4d6f5925d2ed00091e61ae15f8471a1c
nadiwe <nadia.west@hispeed.ch>
2022-02-16 23:50:02 +0100

    breakpoints

--- main.js
+++ main.js
@@ -13,23 +13,5 @@ lottie.loadAnimation({
 
 
 
-var element = document.getElementById('lottie');
 
-lottie.loadAnimation({
-    container: element, // the dom element that will contain the animation
-    renderer: 'svg',
-    loop: false,
-    autoplay: true,
-    path: 'data.json' // the path to the animation json
-});
-
-var element = document.getElementById('lottie2');
-
-lottie.loadAnimation({
-    container: element, // the dom element that will contain the animation
-    renderer: 'svg',
-    loop: false,
-    autoplay: true,
-    path: 'data2.json' // the path to the animation json
-});
 
