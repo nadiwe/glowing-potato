26c7bc2872fd215ef3ca7c3fe92ab6a307d69b2b
nadiwe <nadia.west@hispeed.ch>
2022-02-20 21:47:50 +0100

    main page links to sublinks sections

--- js/index.js
+++ js/index.js
@@ -1,105 +1,82 @@
-
-//document.body.style.backgroundColor = "red";
-
-
-
 $(document).ready(function(){
-    $('.bars').mouseover(function(){ 
-      
-
-        //Add an active class to the anchor
-        $(this).each(function(){
-            let infoId = $(this).context.id;
-            let infoW = '#' + infoId;
-
-            var matches = document.querySelectorAll(infoW);
-            console.log(matches);
-
-    
-          $('div#'+ infoId).addClass('big');
-            $(this).addClass('big');
-           
-          
-     
-    
-         },
-         $('.bars').mouseout(function() {
+  $('.bars').mouseover(function(){ 
             $(this).each(function(){
-                let infoId = $(this).context.id;
-                let infoW = '#' + infoId;
-             
+          let infoId = $(this).context.id
+          /*-----------infos_about_id
+          let infoW = '#' + infoId;
+          var matches = document.querySelectorAll(infoW);
+          console.log(matches);*/  
+        $('div#'+ infoId).addClass('big');
+          $(this).addClass('big');
+       },
+       $('.bars').mouseout(function() {
+          $(this).each(function(){
+              let infoId = $(this).context.id
+               /*-----------infos_about_id
+              let infoW = '#' + infoId;
+              var matches = document.querySelectorAll(infoW);
+              console.log(matches);*/ 
+            $('div#'+ infoId).removeClass('big');
+          $(this).removeClass('big');
 
-                var matches = document.querySelectorAll(infoW);
-                console.log(matches);
-    
-            
-              $('div#'+ infoId).removeClass('big');
-            $(this).removeClass('big');
-
-            })   
-        })
-        
- )}
- )});
-
- /*$(document).ready(function()
-  {
-    $('.banners').hover(
-      function(){ 
-        $(this).width('150%'); //Add an active class to the anchor
-       
-      },
-      function() {
-        $(this).width('100%'); //Remove an active class to the anchor
-
-      }
-   )
- });
-
-
-$(document).ready(function(){
-    $("#discoverImg").toggle(function(){
-      //alert("You entered p1!");
-      $("#discoverImg").width(600);
-
-    
-   // function(){
-    //  alert("Bye! You now leave p1!");
-    }
-    , function() {
-        alert( "Second handler for .toggle() called." );
-      });
+          })   
+      })
+      
+    )
   });
 
+  
 
-document.getElementById('discoverImg').onmouseover = function (evt) {
+$.fn.isInViewport = function() {
+var elementTop = $(this).offset().top;
+var elementBottom = elementTop + $(this).outerHeight();
+var viewportTop = $(window).scrollTop();
+var viewportBottom = viewportTop + $(window).height();
 
-    let 
+return (elementBottom-20)  > viewportTop && elementTop < viewportBottom;
+};
+
+$(window).on('resize scroll', function() {
+$('section').each(function() {
+
+  
+   
+ console.log($(this))
+console.log($(this).isInViewport())
+ 
+  if (!$(this).isInViewport()) {
+ 
+    $('.under div div').css('width','10%');
+
+}
+ else{ 
+
+  console.log('HIIIIIIEEEERRRR')
+
+       // $('.under nav div div').css('width','2%');
+       console.log($(this))
+       let sectionCheck = $(this).context.id
+      let changeNav = 'div#'+sectionCheck +'Nav'
+let changeNavW = $(changeNav).css('width')
+      console.log(sectionCheck)
+        $('div#'+sectionCheck+'Nav').css('width','160%');
+        $('div#_redNav').css('width','160%');
+
+
+    // let widthNav = changeNav.css('width','auto')
+     //console.log(widthNav)
 }
 
+})
+
+});
+
+});
 
 
 
 
-const elementId = document.getElementById('h1');
-
-elementId.style.color = "white";
-elementId.style.width = '200px';
 
 
-const elementImg = document.getElementById('cutoutImg');
-elementImg.style.width = '200px';
-
-
-
-
-var elementClass = document.getElementsByClassName("h1");
-//elementNav.style.width='50px';
-elementClass.style.color = 'red';
-
-elementClass.style.backgroundColor = "red";
-
-
-*/
 
 
